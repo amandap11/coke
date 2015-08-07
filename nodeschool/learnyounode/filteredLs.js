98ac13ccb7ce92dfa85b9b12af029707e2ef105b
Gustavo Pereira <ghapereira@gmail.com>
2015-08-06 22:56:54 -0300

    Ooooooooooooh yeah! Asynch filter is down! Eight more to go! I'm eager to see you commiting often, 'Manda.

--- nodeschool/learnyounode/filteredLs.js
+++ nodeschool/learnyounode/filteredLs.js
@@ -1,4 +1,18 @@
 var fs = require('fs');
+var path = require('path');
 var dirName = process.argv[2];
-var extension = process.argv[3];
-fs.readdir();
+var extension = '.' + process.argv[3];
+
+fs.readdir(dirName, function(err, data){
+    if (err){
+        throw err;
+    }
+
+    var validFiles;
+    d_len = data.length;
+    for(var i = 0; i < d_len; i++){
+        if(path.extname(data[i]) == extension){
+            console.log(data[i]);
+        }
+    }
+});
